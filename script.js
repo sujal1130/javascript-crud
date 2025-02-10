f8bb7d126bb6ab69dc1c7b098f92f9915bf1b511
sujal1130 <sujalsarodiya758@gmail.com>
2025-02-10 15:28:15 +0530

    remuv Table and this time only work add button -js file in click in add button to Redirect in form.html logic add

--- script.js
+++ script.js
@@ -1,11 +1,3 @@
-if (window.location.pathname.)
-
-// Handle the form submission and cancel button in form.html
-if (window.location.pathname.endsWith("form.html")) {
- const cancelButton = document.getElementById('cancelButton');
- 
- // Handle cancel button
- cancelButton.addEventListener('click', () => {
-     window.location.href = "index.html";
- });
-}
+document.getElementById("addbutton").addEventListener("click", function () {
+    window.location.href = "form.html"; // Redirects to form.html
+});
